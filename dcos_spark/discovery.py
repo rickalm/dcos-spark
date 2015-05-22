f83e8bc63dd0a83a6b8734f1e2a442592b0971db
Timothy Chen <tnachen@gmail.com>
2015-05-22 15:16:35 -0700

    Fix spark cli using new dcos library

--- dcos_spark/discovery.py
+++ dcos_spark/discovery.py
@@ -1,43 +1,20 @@
 from __future__ import print_function
+
 import os
 import sys
 
 import requests
 import toml
 
+from dcos import marathon
 
-def get_spark_task():
-    dcos_config = os.getenv("DCOS_CONFIG")
-    if dcos_config is None:
-        print("Please specify DCOS_CONFIG env variable for reading DCOS "
-              "config")
-        sys.exit(1)
-
-    with open(dcos_config) as f:
-        config = toml.loads(f.read())
-
-    marathon = config["marathon"]
-    url = ("http://" + marathon["host"] + ":" + str(marathon["port"]) +
-           "/v2/apps/spark")
-
-    response = requests.get(url, timeout=5)
-
-    if response.status_code >= 200:
-        if 'app' not in response.json():
-            print(response.json()['message'])
-            sys.exit(1)
-
-        return response.json()["app"]
-    else:
-        print("Bad response getting marathon app def. Status code: " +
-              str(response.status_code))
-        sys.exit(1)
-        return ""
+def get_spark_tasks():
+    client = marathon.create_client()
+    return client.get_tasks("spark")
 
 
 def get_spark_webui():
-    spark_task = get_spark_task()
-    tasks = spark_task['tasks']
+    tasks = get_spark_tasks()
 
     if len(tasks) == 0:
         print("Spark cluster task is not running yet.")
@@ -51,8 +28,7 @@ def get_spark_dispatcher():
     if dcos_spark_url is not None:
         return dcos_spark_url
 
-    spark_task = get_spark_task()
-    tasks = spark_task['tasks']
+    tasks = get_spark_tasks()
 
     if len(tasks) == 0:
         print("Spark cluster task is not running yet.")
