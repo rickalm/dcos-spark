3a50635bfa26e1fef100e122f111134761b5f5be
Timothy Chen <tnachen@gmail.com>
2015-08-27 10:44:02 -0700

    Provide a better message around DCOS_CONFIG env variable

--- dcos_spark/discovery.py
+++ dcos_spark/discovery.py
@@ -8,11 +8,18 @@ import toml
 
 from dcos import marathon, util
 
+def check_dcos_config():
+    if os.getenv("DCOS_CONFIG") is None:
+        print("Please set the DCOS_CONFIG environment variable to your DCOS config file path, e.g: ~/.dcos/dcos.toml")
+        sys.exit(1)
+
 def get_spark_webui():
+    check_dcos_config()
     base_url = util.get_config().get('core.dcos_url')
     return base_url + '/service/spark/'
 
 def get_spark_dispatcher():
+    check_dcos_config()
     dcos_spark_url = os.getenv("DCOS_SPARK_URL")
     if dcos_spark_url is not None:
         return dcos_spark_url
