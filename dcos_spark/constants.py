ab4f765198ad13944d0319e20c7663e28127e845
Michael Gummelt <mgummelt@mesosphere.io>
2015-10-07 17:16:04 -0700

    upgrade to use spark-1.5.0-bin-2.4.0-28fb11ce902ca

--- dcos_spark/constants.py
+++ dcos_spark/constants.py
@@ -1,7 +1,7 @@
-version = '0.3.3'
+version = '0.4.0'
 """DCOS Spark version"""
 
-spark_version = "spark-1.5.0-bin-2.4.0"
+spark_version = "spark-1.5.0-bin-2.4.0-28fb11ce902ca"
 
 spark_executor_uri = (
     "http://downloads.mesosphere.com.s3.amazonaws.com/assets/spark/" +
@@ -10,4 +10,4 @@ spark_executor_uri = (
 
 PATH_ENV = 'PATH'
 
-spark_mesos_image = "mesosphere/spark:1.5.0-hadoop2.4"
+spark_mesos_image = "mesosphere/spark:1.5.0-hadoop2.4-28fb11ce902ca"
