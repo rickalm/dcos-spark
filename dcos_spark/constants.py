5f1351607ec780ce652454426b6c1b47767dccae
Michael Gummelt <mgummelt@mesosphere.io>
2015-11-13 15:09:09 -0800

    decouple dcos-spark from spark version

--- dcos_spark/constants.py
+++ dcos_spark/constants.py
@@ -1,8 +1,3 @@
 from dcos_spark.config import *
 
-spark_executor_uri = (
-    "http://downloads.mesosphere.com.s3.amazonaws.com/assets/spark/" +
-    spark_version +
-    ".tgz")
-
 PATH_ENV = 'PATH'
