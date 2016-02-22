ba9dd946ef4662c16e2f43f8352dd887e78dedf0
Michael Gummelt <mgummelt@mesosphere.io>
2016-02-22 08:50:58 -0800

    add newline to fix syntax tests

---
+++ bin/publish_to_s3.sh
@@ -0,0 +1,8 @@
+# required env vars
+#   VERSION (e.g. 0.5.2)
+#   S3_URL (e.g. s3://<bucket>/<path>)
+
+make clean env
+echo -e "version = '${VERSION}'\n" > dcos_spark/config.py
+python setup.py bdist_wheel
+aws s3 cp dist/*.whl "${S3_URL}"
--- bin/start_integration_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Activate the virtual environment so that we can run make
-source env/bin/activate
-
-# Run the default target: E.g. test and packages
-make
