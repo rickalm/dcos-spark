f7e7b27504b5b4c60c05487de28cd45a34f58bf1
Michael Gummelt <mgummelt@mesosphere.io>
2016-06-27 14:33:49 -0700

    set version

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# required env vars
-#   VERSION (e.g. 0.5.2)
-#   S3_URL (e.g. s3://<bucket>/<path>)
+# env vars
+#   PLATFORM (e.g. linux-x86-64)
 
 set -eux -o pipefail
 
-export VERSION=${GIT_BRANCH#refs/tags/}
-export S3_URL=s3://mgummelt-mesos/
+#export VERSION=${GIT_BRANCH#refs/tags/}
+export S3_URL=s3://downloads.mesosphere.io/spark/assets/cli/"${PLATFORM}"
 
-echo -e "version = '${VERSION}'\n" > dcos_spark/version.py
+#echo -e "version = '${VERSION}'\n" > dcos_spark/version.py
 make clean binary
 aws s3 cp dist/dcos-spark "${S3_URL}"
