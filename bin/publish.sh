e6843f974871743db9d5a39e81b360ddb2a6ed5f
Michael Gummelt <mgummelt@mesosphere.io>
2016-06-27 12:57:55 -0700

    publish script

--- bin/publish.sh
+++ bin/publish.sh
@@ -6,6 +6,9 @@
 
 set -eux -o pipefail
 
+export VERSION=${GIT_BRANCH#refs/tags/}
+export S3_URL=s3://mgummelt-mesos/
+
 echo -e "version = '${VERSION}'\n" > dcos_spark/version.py
 make clean binary
 aws s3 cp dist/dcos-spark "${S3_URL}"
