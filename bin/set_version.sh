80c644aca367274b044fb5138c03e6c53255c59e
Rick Alm <rick.alm@uptake.com>
2016-08-03 14:12:22 -0500

    remove ^0 from git tag, replace undefined with SNAPSHOT

--- bin/set_version.sh
+++ bin/set_version.sh
@@ -5,6 +5,7 @@
 
 set -ex -o pipefail
 
-GIT_BRANCH=${GIT_BRANCH:-$(git name-rev --tags --name-only $(git rev-parse HEAD))}
+GIT_BRANCH=${GIT_BRANCH:-$(git name-rev --tags --name-only $(git rev-parse HEAD) | sed -e 's/\^[0-9]$//')}
+GIT_BRANCH=${GIT_BRANCH/undefined/SNAPSHOT}
 export VERSION="${VERSION:-${GIT_BRANCH#refs/tags/}}"
 echo -e "version = '${VERSION}'" > dcos_spark/version.py
